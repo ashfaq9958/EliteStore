e3d49110184e8810628f1bef2faf914580bc08a6
ashfaq9958 <137767028+ashfaq9958@users.noreply.github.com>
2025-08-24 01:57:33 +0530

    changes

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -85,7 +85,7 @@ const Login = ({
                         <input
                           {...field}
                           placeholder="Username or Email"
-                          className="h-11 sm:h-12 w-full pr-10 sm:pr-12 pl-4 sm:pl-5 text-sm sm:text-base text-gray-200 bg-gray-900/70 border border-gray-700 rounded-xl shadow-inner focus:border-purple-500 focus:ring-2 focus:ring-purple-600 focus:outline-none transition-all"
+                          className="h-11 sm:h-12 w-full pr-10 sm:pr-12 pl-4 sm:pl-5 text-sm sm:text-base text-gray-200 bg-gray-900/70 border border-gray-700 rounded-xl shadow-inner focus:border-purple-500 focus:ring-0 focus:ring-purple-600 focus:outline-none transition-all"
                         />
                         <User className="absolute right-3 sm:right-4 top-1/2 -translate-y-1/2 w-4 sm:w-5 h-4 sm:h-5 text-gray-400 pointer-events-none" />
                       </div>
@@ -107,7 +107,7 @@ const Login = ({
                           {...field}
                           type={visible ? "text" : "password"}
                           placeholder="••••••••"
-                          className="h-11 sm:h-12 w-full pr-10 sm:pr-12 pl-4 sm:pl-5 text-sm sm:text-base text-gray-200 bg-gray-900/70 border border-gray-700 rounded-xl shadow-inner focus:border-purple-500 focus:ring-2 focus:ring-purple-600 focus:outline-none transition-all"
+                          className="h-11 sm:h-12 w-full pr-10 sm:pr-12 pl-4 sm:pl-5 text-sm sm:text-base text-gray-200 bg-gray-900/70 border border-gray-700 rounded-xl shadow-inner focus:border-purple-500 focus:ring-0 focus:ring-purple-600 focus:outline-none transition-all"
                         />
                         <button
                           type="button"
